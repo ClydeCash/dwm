2144f04e279725f888b6873d90aab6900cae83a0
Clyde Cash <135058512+ClydeCash@users.noreply.github.com>
2024-09-24 07:30:40 -0300

    Add files via upload

--- config.h
+++ config.h
@@ -59,14 +59,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           	0,         0,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           	1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           	0,         0,          1,           0,        -1 },
-	{ "Gcolor3", NULL,     NULL,           	0,         1,          0,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 	0,         0,          0,           1,        -1 }, /* xev */
-	{ NULL,      "spvim", NULL,		SPTAG(0),  1,          1,           1,        -1 }, 
-	{ NULL,      "spfm",   NULL, 		SPTAG(1),  1,          1,           1,        -1 }, 
+	/* class     	instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    	NULL,     NULL,           	0,         0,          0,           0,        -1 },
+	{ "Firefox", 	NULL,     NULL,           	1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      	NULL,     NULL,           	0,         0,          1,           0,        -1 },
+	{ "Gcolor3", 	NULL,     NULL,           	0,         1,          0,           0,        -1 },
+	{ "Galculator", NULL,     NULL,           	0,         1,          0,           0,        -1 },
+	{ NULL,      	NULL,     "Event Tester", 	0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL,      	"spvim",  NULL,			SPTAG(0),  1,          1,           1,        -1 }, 
+	{ NULL,      	"spfm",   NULL, 		SPTAG(1),  1,          1,           1,        -1 }, 
 };
 
 /* layout(s) */
